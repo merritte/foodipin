e8c6af6090f6c7f73b77d6f07e8b2a78491f76c1
Shay-Jahen Merritte <Shay@Merritte.com>
2016-11-20 09:39:46 -0500

    added circular images and custom prototype cell

---
+++ FoodiPin/RestaurantTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodiPin
+//
+//  Created by Merritte on 11/19/16.
+//  Copyright © 2016 Merritte. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
